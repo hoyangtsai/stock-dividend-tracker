52a775d152d9aee46396a66bdb11341a025f6ff3
Hoyang Tsai <hoyang@duck.com>
2023-09-26 18:36:05 +0800

    Use swr for requesting dividends

--- src/app/page.js
+++ src/app/page.js
@@ -4,24 +4,11 @@ import { useDispatch, useSelector } from 'react-redux';
 import { debounce } from "lodash";
 import { AutoComplete } from "@/components/autocomplete"
 import { useState, useCallback, useEffect } from "react"
-import React from 'react'
-import FullCalendar from '@fullcalendar/react'
-import interactionPlugin from '@fullcalendar/interaction'
-import multiMonthPlugin from '@fullcalendar/multimonth'
-import listPlugin from '@fullcalendar/list';
-import enLocale from '@fullcalendar/core/locales/en-gb'
-import twLocale from '@fullcalendar/core/locales/zh-tw'
-import tippy from 'tippy.js';
-import 'tippy.js/dist/tippy.css';
-import 'tippy.js/animations/perspective.css';
 import { setWatchList } from '@/features/stock/stockSlice';
-import usePrevious from '@/lib/hooks/usePrevious';
-import LoadingMask from '@/components/LoadingMask';
 
 const FUGLE_API = process.env.NEXT_PUBLIC_FUGLE_API_KEY;
 const FUGLE_END_POINT = process.env.NEXT_PUBLIC_FUGLE_END_POINT;
-
-const HOST = 'https://ntp.tshy.me/stock-service';
+import StockCalendar from '@/components/StockCalendar';
 
 const getQuotesByMarket = async (market) => {
   const res = await fetch(`${FUGLE_END_POINT}/snapshot/quotes/${market}`, {
@@ -42,27 +29,10 @@ export default function Home() {
   const [isLoading, setLoading] = useState(false)
   const [stockOptionList, setStockOptionList] = useState([])
   const [allStocks, setAllStocks] = useState([])
-  const [events, setEvents] = useState([
-    { title: 'event 1-1', start: '2023-07-01' },
-    { 
-      title: 'event 1-2',
-      start: '2023-07-02',
-      color: 'blue',
-      description: 'description for Repeating Event'
-    },
-    { 
-      title: 'event 2-1',
-      start: '2023-07-06',
-      color: 'purple',
-      description: 'description for Repeating Event'
-    },
-    { title: 'event 2-2', start: '2023-07-07', },
-  ]);
+
   const stockWatchList = useSelector((state) => state.stock.watchList);
   const dispatch = useDispatch();
   const [currentSymbol, setCurrentSymbol] = useState('');
-  const lastSymbol = usePrevious(currentSymbol);
-  const [isFetching, setIsFetching] = useState(false);
 
   useEffect(() => {
     // fetch all TSE stocks
@@ -93,53 +63,6 @@ export default function Home() {
     }
   }
 
-  // getStockInfo
-  useEffect(() => {
-    // const controller = new AbortController();
-    // const signal = controller.signal;
-
-    const getStockInfo = async (symbol) => {
-      if (symbol) {
-        setIsFetching(true);
-        try {
-          const res = await fetch(`${HOST}/stock/${symbol}`);
-          if (!res.ok) {
-            throw new Error('Failed to fetch');
-          }
-          const response = await res.json();
-          const { data } = response;
-          console.log('data :>> ', data);
-    
-          let eventDates = [];
-          for (const rec of data) {
-            const { CashExDividendTradingDate, CashEarningsDistribution, StockEarningsDistribution } = rec;
-            
-            eventDates.push({
-              title: `${stockWatchList[currentSymbol]?.name || '' }除權息`,
-              start: CashExDividendTradingDate,
-              description: `現金:${CashEarningsDistribution}, 配股:${StockEarningsDistribution}`,
-            })
-          }
-          setEvents(eventDates);
-        } catch (e) {
-          console.log('fetch StockInfo Error:', e);
-          // console.log('signal.aborted:', signal.aborted);
-        } finally {
-          setIsFetching(false);
-        }
-      }
-    };
-  
-    if (lastSymbol !== currentSymbol) {
-      getStockInfo(currentSymbol);
-    }
-
-    return () => {
-      // controller.abort();
-    }
-  }, [lastSymbol, currentSymbol])
-  
-
   const handleStockSearchChange = debounce((value) => {
     if (value) {
       setStockOptions(value);
@@ -154,16 +77,6 @@ export default function Home() {
     setCurrentSymbol(option?.symbol || '');
   }, [dispatch]);
 
-  const handleCalMount = useCallback((item) => {
-    const { description = '' } = item?.event?.extendedProps;
-    if (description) {
-      tippy(item.el, {
-        trigger: 'mouseenter',
-        content: description,
-        animation: 'perspective',
-      })
-    }
-  }, []);
 
   return (
     <div>
@@ -185,36 +98,7 @@ export default function Home() {
         </div>
       </div>
       <div className="mx-auto p-4 max-w-4xl relative">
-        <LoadingMask isVisible={isFetching} />
-        <div className="relative z-10">
-          <FullCalendar
-            plugins={[ interactionPlugin, multiMonthPlugin, listPlugin ]}
-            initialView="multiMonthYearGrid"
-            headerToolbar={{
-              left: 'prev,next today',
-              center: 'title',
-              right: 'multiMonthYearGrid,multiMonthYearStack,listYear'
-            }}
-            views={{
-              multiMonthYearGrid: {
-                type: 'multiMonthYear',
-                buttonText: '網格'
-              },
-              multiMonthYearStack: {
-                type: 'multiMonthYear',
-                buttonText: '堆疊',
-                multiMonthMaxColumns: 1
-              },
-              listYear: {
-                buttonText: '列表顯示'
-              },
-            }}
-            locale={twLocale}
-            locales={[enLocale, twLocale]}
-            events={events}
-            eventDidMount={handleCalMount}
-          />
-        </div>
+        <StockCalendar symbol={currentSymbol} />
       </div>
     </div>
   )
---
+++ src/components/StockCalendar.jsx
@@ -0,0 +1,130 @@
+import { useState, useCallback, useMemo } from 'react'
+import FullCalendar from '@fullcalendar/react'
+import interactionPlugin from '@fullcalendar/interaction'
+import multiMonthPlugin from '@fullcalendar/multimonth'
+import listPlugin from '@fullcalendar/list';
+import enLocale from '@fullcalendar/core/locales/en-gb'
+import twLocale from '@fullcalendar/core/locales/zh-tw'
+import tippy from 'tippy.js';
+import 'tippy.js/dist/tippy.css';
+import 'tippy.js/animations/perspective.css';
+import useSWR from 'swr'
+import { useSelector } from 'react-redux';
+import LoadingMask from '@/components/LoadingMask';
+
+const HOST = 'https://ntp.tshy.me/stock-service';
+
+const dummy = [
+  { title: 'event 1-1', start: '2023-07-01' },
+  { 
+    title: 'event 1-2',
+    start: '2023-07-02',
+    color: 'blue',
+    description: 'description for Repeating Event'
+  },
+  { 
+    title: 'event 2-1',
+    start: '2023-07-06',
+    color: 'purple',
+    description: 'description for Repeating Event'
+  },
+  { title: 'event 2-2', start: '2023-07-07', },
+];
+
+const fetcher = (url) => fetch(url).then(r => r.json());
+
+const StockCalendar = ({ symbol }) => {
+  const stockWatchList = useSelector((state) => state.stock.watchList);
+
+  const [events, setEvents] = useState([]);
+
+  const handleCalMount = useCallback((item) => {
+    const { description = '' } = item?.event?.extendedProps;
+    if (description) {
+      tippy(item.el, {
+        trigger: 'mouseenter',
+        content: description,
+        animation: 'perspective',
+      })
+    }
+  }, []);
+
+  const { data: resp, error, isLoading } = useSWR(symbol != '' ? `${HOST}/stock/${symbol}` : null, fetcher);
+
+  useMemo(() => {    
+    if (!resp?.data || events.some(item => item.symbol == symbol)) return;
+    
+    const { data } = resp;
+
+    let eventDates = [];
+    for (const r of data) {
+      const { CashExDividendTradingDate, CashEarningsDistribution, StockExDividendTradingDate, StockEarningsDistribution } = r;
+      
+      let roundCash = (Math.round(CashEarningsDistribution * 10000) / 10000),
+          roundStock = (Math.round(StockEarningsDistribution * 10000) / 10000);
+
+      if (CashEarningsDistribution == 0 && StockEarningsDistribution > 0) {
+        eventDates.push({
+          title: `${stockWatchList[symbol]?.name || '' }除權`,
+          start: StockExDividendTradingDate,
+          description: `配股:${roundStock}`,
+          symbol: symbol,
+        });
+      } else if (StockEarningsDistribution == 0 && CashEarningsDistribution > 0) {
+        eventDates.push({
+          title: `${stockWatchList[symbol]?.name || '' }除息`,
+          start: CashExDividendTradingDate,
+          description: `現金:${roundCash}`,
+          symbol: symbol,
+        });
+      } else if (CashEarningsDistribution > 0 && StockEarningsDistribution > 0) {
+        eventDates.push({
+          title: `${stockWatchList[symbol]?.name || '' }除權息`,
+          // title: `除權息`,
+          start: CashExDividendTradingDate,
+          description: `現金:${roundCash}, 配股:${roundStock}`,
+          symbol: symbol,
+        });
+      }
+    }
+
+    setEvents((prev) => [...prev, ...eventDates]);
+  }, [events, resp]);
+
+  return (
+    <div className="relative">
+      <LoadingMask isVisible={isLoading} />
+      <div className="relative z-10">
+        <FullCalendar
+          plugins={[ interactionPlugin, multiMonthPlugin, listPlugin ]}
+          initialView="multiMonthYearGrid"
+          headerToolbar={{
+            left: 'prev,next today',
+            center: 'title',
+            right: 'multiMonthYearGrid,multiMonthYearStack,listYear'
+          }}
+          views={{
+            multiMonthYearGrid: {
+              type: 'multiMonthYear',
+              buttonText: '網格'
+            },
+            multiMonthYearStack: {
+              type: 'multiMonthYear',
+              buttonText: '堆疊',
+              multiMonthMaxColumns: 1
+            },
+            listYear: {
+              buttonText: '列表顯示'
+            },
+          }}
+          locale={twLocale}
+          locales={[enLocale, twLocale]}
+          events={events}
+          eventDidMount={handleCalMount}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default StockCalendar
